6ac899be38dc1942cdc901f7f35d35bdd600dd00
sangita avghad <sangita.awaghad@gmail.com>
2020-05-25 03:53:00 -0700

    ADDED:image for showing success

--- components/GameOverScreen.js
+++ components/GameOverScreen.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {View,Text,StyleSheet,Button} from 'react-native';
+import {View,Text,StyleSheet,Button,Image} from 'react-native';
 import Colors from '../constants/color'
 const GameOverScreen = props =>{
     return(
         <View style={styles.screen}>
             <Text style={styles.gameOverText} color={Colors.secondary}>Guess Number is {props.guessNumber}</Text>
             <Text style={styles.gameOverText}>The Game is Over!</Text>
-            
+            <View style={styles.imageContainer}>
+                <Image 
+                    source={require('../assets/images.jpeg')}
+                    style={styles.image}/>
+            </View>
             <Button title="Start a New Game" 
                     color={Colors.secondary}
                     onPress={props.onRestart}/>
@@ -22,6 +26,18 @@ const styles = StyleSheet.create({
     },
     gameOverText:{
         fontSize:26
+    },
+    imageContainer:{
+        width:300,
+        height:300,
+        borderBottomColor:'black',
+        borderBottomWidth:5,
+        borderRadius:150,
+        overflow:'hidden'
+    },
+    image:{
+        width:'100%',
+        height:'100%'
     }
 });
 
